19276789866248688e1c3d00e42080600cc11c77
Keny2802 <131550477+Keny2802@users.noreply.github.com>
2025-07-12 13:56:58 +0200

    Add files via upload

---
+++ 1/home.js
@@ -0,0 +1,87 @@
+let isOpened = false;
+
+const body = document.body;
+
+const mobileMenu = document.querySelector(".mobile-header");
+const siteToggle = document.querySelector(".menu");
+const mobileMenuToggle = document.querySelector(".mobile-header-icon");
+const serviceContainer = document.querySelector(".services-wrapper");
+const clientsChooseCTA = document.querySelector(".client-choose-property");
+const clientsSelectContainer = document.querySelector(".clients-select-container");
+
+function customSiteNavLinks() {
+    const siteNavLinks = document.querySelectorAll(".nav-link");
+
+    siteNavLinks.forEach((siteNavLink) => {
+        siteNavLink.addEventListener("click", function(evt) {
+            evt.preventDefault();
+
+            const target = evt.target;
+            const targetID = target.getAttribute("href").substring(1);
+            const targetLink = document.getElementById(targetID);
+
+            if (targetLink) {
+                targetLink.scrollIntoView({
+                    behavior: "smooth"
+                });
+            };
+        });
+    });
+};
+
+function customMobileNavLinks() {
+    const mobileNavLinks = mobileMenu.querySelectorAll(".mobile-nav-link");
+
+    mobileNavLinks.forEach((mobileNavLink) => {
+        mobileNavLink.addEventListener("click", function(evt) {
+            evt.preventDefault();
+            mobileMenu.classList.remove("show");
+
+            const target = evt.target;
+            const targetID = target.getAttribute("href").substring(1);
+            const targetLink = document.getElementById(targetID);
+
+            if (targetLink) {
+                targetLink.scrollIntoView({
+                    behavior: "smooth"
+                });
+            };
+
+            body.style.overflowY = !mobileMenu.classList.contains("show") ? "auto" : "hidden";
+        });
+    });
+};
+
+function customServicePhotos() {
+    const servicePhotos = serviceContainer.querySelectorAll(".job-service-photo");
+
+    servicePhotos.forEach((servicePhoto, servicePhotoIndex) => {
+        servicePhoto.setAttribute("draggable", "false");
+        servicePhoto.style.userSelect = "none";
+    });
+};
+
+siteToggle.addEventListener("click", function(evt) {
+    mobileMenu.classList.add("show");
+
+    body.style.overflowY = mobileMenu.classList.contains("show") ? "hidden" : "auto";
+});
+
+mobileMenuToggle.addEventListener("click", function(evt) {
+    mobileMenu.classList.remove("show");
+
+    body.style.overflowY = !mobileMenu.classList.contains("show") ? "auto" : "hidden";
+});
+
+clientsChooseCTA.addEventListener("click", function(evt) {
+    isOpened = !isOpened;
+    evt.preventDefault();
+
+    clientsSelectContainer.style.opacity = isOpened ? "1" : "0";
+});
+
+document.addEventListener("DOMContentLoaded", function(evt) {
+    customSiteNavLinks();
+    customMobileNavLinks();
+    customServicePhotos();
+});
