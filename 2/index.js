4e10d955c1bce9ee62266e11fc332f0c93d195da
Keny2802 <131550477+Keny2802@users.noreply.github.com>
2025-08-01 17:13:54 +0200

    Add files via upload

--- 2/index.js
+++ 2/index.js
@@ -1,11 +1,15 @@
 let lineIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
+let carouselIndex = 0;
 
 const header = document.getElementById("header");
 const headerLogo = header.querySelector(".logo-content");
 const headerLinks = document.querySelectorAll(".navigation-link");;
+const sectionCtaBTNS = document.querySelectorAll(".section-cta-btn");
 const hamburger = document.getElementById("hamburger");
+const toggleThemeBTN = document.getElementById("theme-toggle");
+const toggleThemeBtnICON = document.getElementById("theme-icon");
 const menu = document.getElementById("mobile-menu-header");
 const menuLinks = menu.querySelectorAll(".mobile-menu-link");
 const hero = document.getElementById("hero");
@@ -14,6 +18,11 @@ const monthlyBTN = document.getElementById("monthly-tab");
 const annuallyBTN = document.getElementById("annually-tab");
 const monthlyPlans = document.getElementById("monthly-plans");
 const annuallyPlans = document.getElementById("annually-plans");
+const faqToggles = document.querySelectorAll(".faq-toggle");
+const carousel = document.getElementById("review-carousel");
+const carouselPrevBTN = document.getElementById("controls-prev-btn");
+const carouselNextBTN = document.getElementById("controls-next-btn");
+const totalCarouselItems = carousel.children.length;
 
 headerLinks.forEach((headerLink) => {
     headerLink.addEventListener("click", function(evt) {
@@ -57,6 +66,22 @@ menuLinks.forEach((menuLink) => {
     });
 });
 
+sectionCtaBTNS.forEach((sectionCtaBTN) => {
+    sectionCtaBTN.addEventListener("click", function(evt) {
+        evt.preventDefault();
+
+        const target = evt.target;
+        const targetID = target.getAttribute("href").substring(1);
+        const targetLink = document.getElementById(targetID);
+
+        if (targetLink) {
+            targetLink.scrollIntoView({
+                behavior: "smooth"
+            });
+        };
+    });
+});
+
 monthlyBTN.addEventListener("click", function(evt) {
     monthlyPlans.classList.remove("hidden");
     annuallyPlans.classList.add("hidden");
@@ -71,11 +96,51 @@ annuallyBTN.addEventListener("click", function(evt) {
     monthlyBTN.classList.remove("active-tab")
 });
 
+faqToggles.forEach((faqToggle, faqToggleIndex) => {
+    faqToggle.addEventListener("click", function(evt) {
+        const faqContent = faqToggle.nextElementSibling;
+        const faqIcon = faqToggle.querySelector(".faq-svg");
+
+        // zavřít ostatní
+        faqToggles.forEach((otherFaqToggle) => {
+            if (otherFaqToggle !== faqToggle) {
+                otherFaqToggle.nextElementSibling.classList.add("hidden");
+                otherFaqToggle.querySelector(".faq-svg").classList.remove("rotate-180");
+            };
+        });
+
+        faqContent.classList.toggle("hidden");
+        faqIcon.classList.toggle("rotate-180");
+    });
+});
+
+carouselNextBTN.addEventListener("click", function(evt) {
+    carouselIndex = (carouselIndex + 1) % totalCarouselItems;
+    updateCarousel();
+});
+
+carouselPrevBTN.addEventListener("click", function(evt) {
+    carouselIndex = (carouselIndex - 1 + totalCarouselItems) % totalCarouselItems;
+    updateCarousel();
+});
+
+setInterval(() => {
+    carouselIndex = (carouselIndex + 1) % totalCarouselItems;
+    updateCarousel();
+}, 10000);
+
 const tagLines = [
     "Bez stresu a papírování.",
     "Vše na jednom místě.",
     "Vaši nájemníci Vám poděkují.",
-    "Rychlé vyúčtování jedním klikem"
+    "Rychlé vyúčtování jedním klikem",
+
+    "Automatické upomínky a platby.",
+    "Správa nájmů nikdy nebyla jednodušší.",
+    "Měj přehled o každé nemovitosti.",
+    "Jednoduché rozhraní, silné funkce.",
+    "Ušetři čas, energii i starosti.",
+    "Profesionální nástroj pro správce a pronajímatele."
 ];
 
 function typeTagLine() {
@@ -94,12 +159,18 @@ function typeTagLine() {
         }, 1000);
     } else if (isDeleting && charIndex === 0) {
         isDeleting = false;
+        // lineIndex = (lineIndex + 1) % tagLines.length;
         lineIndex = (lineIndex + 1) % tagLines.length;
     };
 
     const delay = isDeleting ? 40 : 80;
 
     setTimeout(typeTagLine, delay);
+};
+
+function updateCarousel() {
+    const offset = -carouselIndex * 100;
+    carousel.style.transform = `translateX(${offset}%)`;
 };
 
 typeTagLine();
