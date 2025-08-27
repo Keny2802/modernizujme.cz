1701eb896e6845b637f23fc2d5fa64a7ce89be1e
Keny2802 <131550477+Keny2802@users.noreply.github.com>
2025-08-27 18:41:17 +0200

    Delete src directory

--- src/components/FooterRights.tsx
+++
@@ -1,9 +0,0 @@
-export default function FooterRights() {
-    const currentYear = new Date().getFullYear();
-    
-    return (
-        <span className="side-panel-text-content-dynamic-year-right-wrapper dynamic-year">
-            {currentYear}
-        </span>
-    );
-};
